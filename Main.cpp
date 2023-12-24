6f3c456d4928428f54a6b27280005fab60d4cbe7
靖钊 黄 <Huang-Jingzhao@outlook.com>
2023-12-24 22:17:38 +0800

    v0.010

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,58 @@
 ﻿#include <iostream>
+#include <Windows.h>
+#include "BadMouse.h"
+const string version = "0.010";
 using namespace std;
+void e()
+{
+	bool f = 0;
+	SetWinSize(20, 5);
+	print("按Scroll Lock开始……",0,2);
+	while (1)
+	{
+		
+		if (eventKey(VK_SCROLL))
+		{
+			f = !f;
+			clears();
+			//HideCursor();
+			if (f) print("状态：■", 5, 2);
+			else print("状态：□", 5, 2);
+			while (eventKey(VK_SCROLL));
+		}
+		if (f) Click(LeftClick);
+		Sleep(50);
+	}
+}
+
 int main()
 {
-    cout << "Hello Bad Mouse!\n";
+	SetWinSize(31, 10);
+	SetTitle("Bad Mouse");
+	HideCursor();
+	print("坏掉的鼠标",YELLOW, 10, 2);
+	print(" by 黄靖钊", 21, 9);
+	print("v", 25, 8); print(version);
+	print("任意键继续……",GREEN, 0, 9);
+	{int a=_getch();}
+
+	SetWinSize(51, 10);
+
+	print("功能列表", 21, 1);
+	print("1.鼠标连点", 5, 5);
+	//print("H.帮助与介绍", 0, 9);
+	//print("S.设置", 45,9);
+	eventLoop({ '1' }, {e});
+
+	while (1) {                      			//循环检测
+
+		
+		if (eventKey(VK_LBUTTON)) {  			//鼠标左键按下
+			printf("key down!\n");
+			while (eventKey(VK_LBUTTON));
+			return 0;
+		}
+		Sleep(20);                  			//等待20毫秒，减少CPU占用
+	}
+    
 }
